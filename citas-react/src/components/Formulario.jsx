48b2eb7cef99cf9c4838f4941c0c665fd2f2e7d1
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-18 22:35:07 -0500

    tailwind implementation

---
+++ citas-react/src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+
+function Formulario() {
+  return (
+    <div>
+      <h2>Formulario</h2>
+    </div>
+  )
+}
+
+export default Formulario
+
---
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -0,0 +1,8 @@
+
+function ListadoPacientes() {
+  return (
+      <h2>Listado pacientes</h2>
+  )
+}
+
+export default ListadoPacientes
