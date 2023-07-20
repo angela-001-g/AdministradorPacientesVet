69ec408825828527fc3b29bcc5f2030158abb1ff
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-19 23:53:59 -0500

    pacientes

---
+++ citas-react/src/components/Paciente.jsx
@@ -0,0 +1,35 @@
+function Paciente() {
+  return (
+    <div className="m-3 mt-5 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {''}
+            <span className="font-normal normal-case">Sabado</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {''}
+            <span className="font-normal normal-case">Daniel</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {''}
+            <span className="font-normal normal-case">correo@correo.com</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta: {''}
+            <span className="font-normal normal-case">10 Diciembre de 2023</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+        Sintomas: {''}
+            <span className="font-normal normal-case">Lorem, ipsum dolor sit amet consectetur adipisicing  
+            elit. Architecto facere, numquam vero quo tempore ullam repellendus sequi corrupti sint alias
+            non inventore velit, blanditiis temporibus nesciunt nostrum cumque. Et, labore.
+            </span>
+        </p>      
+    </div>
+  )
+}
+
+export default Paciente
