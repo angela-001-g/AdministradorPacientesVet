02609cb5f4e93987bda251e7ff5349fc671a0208
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-07-26 23:52:04 -0500

    changes

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,19 +1,19 @@
-import {useState} from "react"
+import { useState } from "react"
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
-import ListadoPacientes from "./components/ListadoPacientes" 
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
-
+  let pacientes = false
   const [pacientes, setPacientes] = useState([])
 
   return (
     <div className="container mx-auto mt-20 ">
-      <Header 
-       numeros={ 1 }
+      <Header
+        numeros={1}
       />
       <div className="mt-12 md:flex">
-        <Formulario /> 
+        <Formulario />
         <ListadoPacientes />
       </div>
     </div>
